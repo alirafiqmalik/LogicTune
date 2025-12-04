b4f8b0148207af7228959d31c02dd7115b84faae
Ali Hamza Malik <alirafiqmalik@gmail.com>
2025-12-04 13:56:40 -0500

    Updated prompt formatting. Changes to be committed: 	modified:   src/logictune/data_generator.py 	modified:   src/logictune/evaluator.py 	modified:   src/logictune/trainer.py 	modified:   src/logictune/training_tracker.py

--- src/logictune/training_tracker.py
+++ src/logictune/training_tracker.py
@@ -131,7 +131,7 @@ class DPOMetricsCallback(TrainerCallback):
                     # Format prompt
                     output_format= "Generate a response that aligns with the defined Boolean Propositions {    green_traffic_light,green_left_turn_light,opposite_car,car_from_left,car_from_right,pedestrian_at_left,pedestrian_at_right,pedestrian_in_front,side_car,stop_sign} and Actions {stop, go_straight, turn_left, turn_right}"
                     if "TinyLlama" in self.model_name:
-                        formatted = f"<|system|>\nYou are a helpful assistant. Always answer as helpfully as possible, while being safe. Your answers should be detailed. </s>\n<|user|>\n{prompt}</s>\n${output_format}\n</s>\n<|assistant|>\n"
+                        formatted = f"<|system|>\nYou are a helpful assistant. Always answer as helpfully as possible, while being safe. Your answers should be detailed. </s>\n<|user|>\n{prompt}</s>\n<|output|>\n${output_format}</s>\n<|assistant|>\n"
                     else:
                         formatted = f"### Instruction:\n{prompt}\n\n### Response:\n"
                     
